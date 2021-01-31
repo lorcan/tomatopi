04d977e1d2d8b3d6526468c285c942a8b38e1374
Lorcan Coyle <lorcan@gmail.com>
2021-01-31 20:12:51 +0000

    Add more detail to the pijuice status output
    And flatten the output.

--- scripts/pi_juice.py
+++ scripts/pi_juice.py
@@ -2,6 +2,23 @@
 import json
 from pijuice import PiJuice # Import pijuice module
 
+def getData(response):
+    if response['error'] != 'NO_ERROR':
+        raise RuntimeError("PiJuice Error: " + response['error']) 
+    return response['data']
+
 pijuice = PiJuice(1, 0x14) # Instantiate PiJuice interface object
-print(json.dumps(pijuice.status.GetStatus()))
-# print(json.dumps(pijuice.status.GetChargeLevel()))
+
+data = {}
+data.update(getData(pijuice.status.GetStatus()))
+data.update(getData(pijuice.status.GetFaultStatus()))
+data.update(getData(pijuice.status.GetButtonEvents()))
+
+data.update({'chargeLevel': getData(pijuice.status.GetChargeLevel())})
+data.update({'batteryTemperature': getData(pijuice.status.GetBatteryTemperature())})
+data.update({'batteryVoltage': getData(pijuice.status.GetBatteryVoltage())})
+data.update({'batteryCurrent': getData(pijuice.status.GetBatteryCurrent())})
+data.update({'ioVoltage': getData(pijuice.status.GetIoVoltage())})
+data.update({'ioCurrent': getData(pijuice.status.GetIoCurrent())})
+
+print(json.dumps(data))
